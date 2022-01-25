be8862b1fb86149636d619a91a6ec458af90e751
Michael Villeneuve <m.l.villeneuve@uu.nl>
2022-01-25 17:30:37 +0100

    Registrations home and RegistrationCreateView added

---
+++ registrations/forms.py
@@ -0,0 +1,46 @@
+from django.utils.translation import ugettext_lazy as _
+from django import forms
+from django.urls import reverse
+
+from uil.questions import questions
+from .models import Registration
+
+
+
+class RegistrationQuestionMixin:
+
+    def get_edit_url(self):
+
+        if not hasattr(self.instance, 'track'):
+            self.instance.track = self.instance
+
+        return reverse(
+            'tracks:edit_question',
+            kwargs={
+                'question': self.slug,
+                'question_pk': self.instance.pk,
+                'track_pk': self.instance.track.pk,
+            }
+        )
+
+
+class NewRegistrationQuestion(RegistrationQuestionMixin,
+        questions.Question,):
+
+    title = _("registrations:forms:registration_title")
+    model = Registration
+    slug = 'new_track'
+    is_editable = True
+
+    class Meta:
+        model = Registration
+        fields = [
+            'title',
+        ]
+
+    def get_segments(self):
+
+        segments = []
+        segments.append(self._field_to_segment('title'))
+
+        return segments
--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Registration
+from .forms import NewRegistrationQuestion
 
 # Create your views here.
+
+
+class RegistrationsHomeView(generic.TemplateView):
+
+    template_name = 'registrations/home.html'
+
+
+class RegistrationCreateView(generic.CreateView):
+
+    model = Registration
+    template_name = 'registrations/new_registration.html'
+    form_class = NewRegistrationQuestion
