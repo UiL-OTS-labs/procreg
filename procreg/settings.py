f5e189a340af1b94bce62ccf772e55c48e2ad736
Michael Villeneuve <m.l.villeneuve@uu.nl>
2022-02-10 16:22:25 +0100

    Merge translations files

--- procreg/settings.py
+++ procreg/settings.py
@@ -8,6 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -160,10 +161,20 @@ PASSWORD_HASHERS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'nl'
+LANGUAGE_CODE = 'en'
 LANGUAGES = (
     ('nl', _('lang:nl')),
     ('en', _('lang:en')),
+)
+
+
+LOCALE_PATHS = (
+
+    # Merges all translations files into one
+    os.path.join(BASE_DIR, 'locale'),
+
+    # For per-app translations files
+    #'locale',
 )
 
 TIME_ZONE = 'UTC'
