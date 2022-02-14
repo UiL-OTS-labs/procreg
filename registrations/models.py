7be75bd6075a73dd3ca6f00e1717c6629688f3ed
Michael Villeneuve <m.l.villeneuve@uu.nl>
2022-02-14 13:38:19 +0100

    Add new Registration model fields

--- registrations/models.py
+++ registrations/models.py
@@ -1,10 +1,29 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+USER_MODEL = get_user_model()
 
 
 class Registration(models.Model):
 
     title = models.CharField(max_length=200,
                              help_text="models:registration_title_help_text",
-    )
+                             )
+    created_by = models.ForeignKey(USER_MODEL,
+                                   default=None,
+                                   on_delete=models.SET_DEFAULT,
+                                   related_name="registrations_created",
+                                   )
+    created_on = models.DateTimeField(auto_now=True)
+    applicants = models.ManyToManyField(USER_MODEL,
+                                        default=None,
+                                        related_name="applicant_for",
+                                        )
+    date_start = models.DateField(blank=True,
+                                  null=True,
+                                  )
+    date_end = models.DateField(blank=True,
+                                null=True,
+                                )
