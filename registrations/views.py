a5a2e4a99423001c558ba46a09114312839a43be
Michael Villeneuve <m.l.villeneuve@uu.nl>
2022-02-15 17:17:58 +0100

    Delete view

--- registrations/views.py
+++ registrations/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 
+from uil.questions.views import BlueprintView, QuestionEditView
+
 from .models import Registration
 from .forms import NewRegistrationQuestion
+from .blueprints import RegistrationBlueprint
+
 
 # Create your views here.
 
@@ -18,7 +22,24 @@ class RegistrationsHomeView(generic.ListView):
         qs = Registration.objects.all()
         return qs
 
+class RegistrationOverview(BlueprintView):
 
+    blueprint = RegistrationBlueprint
+    pk_url_kwarg = "reg_pk"
+    template_name = "registrations/overview.html"
+
+    def get_object(self,):
+
+        return self.model.objects.get(
+            pk=self.kwargs.get(self.pk_url_kwarg))
+
+class RegistrationQuestionEditView(QuestionEditView,):
+
+    def get_success_url(self):
+
+        return reverse('registrations:overview',
+                       kwargs={'reg_pk': self.kwargs.get('reg_pk')}
+        )
 
 
 class RegistrationCreateView(generic.CreateView):
@@ -27,3 +48,10 @@ class RegistrationCreateView(generic.CreateView):
     template_name = 'registrations/new_registration.html'
     form_class = NewRegistrationQuestion
     success_url = reverse_lazy("registrations:home")
+
+class RegistrationDeleteView(generic.DeleteView):
+
+    model = Registration
+    template_name = "registrations/delete_registration.html"
+    success_url = reverse_lazy("registrations:home")
+    pk_url_kwarg = 'reg_pk'
