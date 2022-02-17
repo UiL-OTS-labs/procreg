3412092399829582bfe8c271e0b642a2207e9207
Michael Villeneuve <m.l.villeneuve@uu.nl>
2022-02-17 16:50:41 +0100

    New and improved questions

--- registrations/forms.py
+++ registrations/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.urls import reverse
 
 from uil.questions import questions
-from .models import Registration
+from .models import Registration, ParticipantCategory
 
 
 
@@ -11,15 +11,15 @@ class RegistrationQuestionMixin:
 
     def get_edit_url(self):
 
-        if not hasattr(self.instance, 'track'):
-            self.instance.track = self.instance
+        if not hasattr(self.instance, 'registration'):
+            self.instance.registration = self.instance
 
         return reverse(
-            'tracks:edit_question',
+            'registrations:edit_question',
             kwargs={
                 'question': self.slug,
                 'question_pk': self.instance.pk,
-                'track_pk': self.instance.track.pk,
+                'reg_pk': self.instance.registration.pk,
             }
         )
 
@@ -29,7 +29,7 @@ class NewRegistrationQuestion(RegistrationQuestionMixin,
 
     title = _("registrations:forms:registration_title")
     model = Registration
-    slug = 'new_track'
+    slug = 'new_reg'
     is_editable = True
 
     class Meta:
@@ -45,5 +45,47 @@ class NewRegistrationQuestion(RegistrationQuestionMixin,
 
         return segments
 
+class FacultyQuestion(RegistrationQuestionMixin, questions.Question):
 
-QUESTIONS = {'new_registration': NewRegistrationQuestion}
+    class Meta:
+        model = Registration
+        fields = [
+            'faculty',
+        ]
+
+    title = _("registrations:forms:faculty_question_title")
+    model = Registration
+    slug = "faculty"
+    is_editable = True
+
+
+    def get_segments(self):
+
+        segments = []
+        segments.append(self._field_to_segment('faculty'))
+
+        return segments
+
+
+class CategoryQuestion(RegistrationQuestionMixin, questions.Question):
+
+    class Meta:
+        model = ParticipantCategory
+        fields = [
+            'name',
+            'number',
+            'has_consented',
+        ]
+
+    is_editable = True
+    slug = "category"
+    model = ParticipantCategory
+
+
+
+
+Q_LIST = [NewRegistrationQuestion,
+          FacultyQuestion,
+          ]
+
+QUESTIONS = {q.slug: q for q in Q_LIST}
