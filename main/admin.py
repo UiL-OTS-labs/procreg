7a4375878614e4bd07527d096af9267e1fde9ebf
Michael Villeneuve <m.l.villeneuve@uu.nl>
2022-04-19 12:56:17 +0200

    Allow user model to be edited in django-admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+AUTH_USER_MODEL = get_user_model()
 
 # Register your models here.
+
+
+admin.site.register(AUTH_USER_MODEL)
