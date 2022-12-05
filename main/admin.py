bd554b4b5616ef11aa3a1d298081872a72fd3b75
Michael Villeneuve <m.l.villeneuve@uu.nl>
2022-12-05 13:51:43 +0100

    Fix user model not using correct admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 AUTH_USER_MODEL = get_user_model()
 
 # Register your models here.
 
 
-admin.site.register(AUTH_USER_MODEL)
+admin.site.register(AUTH_USER_MODEL, UserAdmin)
