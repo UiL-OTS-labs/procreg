91cb0336ce0127b27ec32550e589f1d0f50d6f8f
Michael Villeneuve <m.l.villeneuve@uu.nl>
2023-01-24 12:45:04 +0100

    Register Involved model for admin site

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Registration
+from .models import Registration, Involved
 
 # Register your models here.
 
 admin.site.register(Registration)
+
+admin.site.register(Involved)
