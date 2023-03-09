7778d8381ab0e95e8bfed83387a6a8ab92d4b526
Michael Villeneuve <m.l.villeneuve@uu.nl>
2023-03-09 15:30:19 +0100

    Involved and other models, with migrations

--- registrations/models/__init__.py
+++ registrations/models/__init__.py
@@ -1,3 +1,4 @@
 from .registration import Registration
 from .involved import Involved
 from .minor_models import Receiver, ParticipantCategory, Software
+from .details import SpecialDetail
--- registrations/models/involved.py
+++ registrations/models/involved.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
 
 from . import Registration
+from .details import SpecialDetail, SensitiveDetail, SocialMediaDetail, \
+    ExtraDetail, RegularDetail, ICFormDetail
 from .registration import YES_NO, YES_NO_NA
 
 # Create your models here.
@@ -40,15 +42,15 @@ class Involved(models.Model):
     )
     # Fields relating to personal information
     special_details = models.ManyToManyField(
-        # SpecialDetail,
-        on_delete=models.SET_DEFAULT,
+        to=SpecialDetail,
     )
-    gave_explicit_permission = models.CharField(
-        max_length=10,
-        choices=YES_NO,
+    gave_explicit_permission = models.BooleanField(
+        null=True,
+        default=None,
     )
     explicit_permission_details = models.TextField(
         max_length=1000,
+        default="",
     )
     GROUNDS = (
         ("public", _("models:involved:public_grounds")),
@@ -58,57 +60,54 @@ class Involved(models.Model):
     grounds_for_exception = models.CharField(
         max_length=25,
         choices=GROUNDS,
+        default="",
     )
     algemeen_belang_details = models.TextField(
         max_length=1000,
+        default="",
     )
     legal_grounds_details = models.TextField(
         max_length=1000,
+        default="",
     )
     # Sensitive personal details
-    provides_criminal_information = models.CharField(
-        max_length=25,
-        choices=YES_NO,
+    provides_criminal_information = models.BooleanField(
+        null=True,
+        default=None,
     )
-    government_supervised_collection = models.CharField(
-        max_length=25,
-        choices=YES_NO,
+    government_supervised_collection = models.BooleanField(
+        null=True,
+        default=None,
     )
-    involves_children_under_15 = models.CharField(
-        max_length=25,
-        choices=YES_NO,
+    involves_children_under_15 = models.BooleanField(
+        null=True,
+        default=None,
     )
-    confirm_parental_permission = models.CharField(
-        max_length=25,
-        choices=YES_NO,
+    confirm_parental_permission = models.BooleanField(
+        null=True,
+        default=None,
     )
     other_sensitive_details = models.ManyToManyField(
-        #SensitiveDetail,
-        on_delete=models.SET_DEFAULT,
+        SensitiveDetail
     )
-    provides_no_sensitive_details = models.CharField(
-        max_length=25,
-        choices=YES_NO,
+    provides_no_sensitive_details = models.BooleanField(
+        null=True,
+        default=None,
     )
     # Regular ol' details
     regular_details = models.ManyToManyField(
-        #RegularDetail,
-        on_delete=models.SET_DEFAULT,
+        RegularDetail,
     )
     social_media_details = models.ManyToManyField(
-        #SocialMediaDetail,
-        on_delete=models.CASCADE,
+        SocialMediaDetail,
     )
     extra_details = models.ManyToManyField(
-        #ExtraDetail,
-        on_delete=models.CASCADE,
+        ExtraDetail,
     )
-    
-        
-
-
-class InformationKind(models.Model):
-    description = models.CharField(
-        max_length=200,
-        default="",
+    provides_ic_form = models.BooleanField(
+        null=True,
+        default=None,
+    )
+    ic_form_details = models.ManyToManyField(
+        ICFormDetail,
     )
