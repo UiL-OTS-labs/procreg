a73068694b6a253648f6e959d0c3c20232c8d914
Michael Villeneuve <m.l.villeneuve@uu.nl>
2023-04-18 17:58:34 +0200

    Make landing new default frontpage

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,7 @@ from registrations.views import LandingView
 app_name = 'main'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', LandingView.as_view(), name='home'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
