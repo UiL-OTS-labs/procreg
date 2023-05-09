76fdabd7a062d45e48b1ce2d6ec22064e84cde02
Michael Villeneuve <m.l.villeneuve@uu.nl>
2023-05-09 17:41:32 +0200

    Make home point to new registrations list

--- registrations/urls.py
+++ registrations/urls.py
@@ -4,7 +4,7 @@ from .views import RegistrationsHomeView, RegistrationCreateView, \
     RegistrationOverview, RegistrationQuestionEditView, RegistrationDeleteView, \
     MinimalCategoryView, MinimalDeleteView, RegistrationSummaryView, \
     InvolvedManager, StepperView, BlueprintQuestionEditView, \
-    ReceiverDeleteView, SoftwareDeleteView, LandingView
+    ReceiverDeleteView, SoftwareDeleteView, LandingView, MyRegistrationsList
 from .questions import QUESTIONS
 from .blueprints import RegistrationBlueprint
 from .models import ParticipantCategory
@@ -12,7 +12,8 @@ from .models import ParticipantCategory
 app_name = 'registrations'
 
 urlpatterns = [
-    path('', RegistrationsHomeView.as_view(), name='home'),
+    path('', MyRegistrationsList.as_view(), name='my_list'),
+    path('home/', RegistrationsHomeView.as_view(), name="home"),
     path('landing/', LandingView.as_view(), name='landing'),
     path('<int:reg_pk>/', RegistrationOverview.as_view(), name='overview'),
     path('<int:reg_pk>/manager/',
