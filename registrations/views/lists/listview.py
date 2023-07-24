2549cd058e220de2fd3da4b444b197db1672bc03
Michael Villeneuve <m.l.villeneuve@uu.nl>
2023-07-24 15:48:37 +0200

    DSC and Django 4 compatibility

--- registrations/views/lists/listview.py
+++ registrations/views/lists/listview.py
@@ -1,7 +1,7 @@
 from django.views import generic
 from django.db.models import Q
 from django.urls import reverse
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 from django.contrib.auth.mixins import LoginRequiredMixin, \
     UserPassesTestMixin, PermissionDenied
 from django.core.exceptions import ImproperlyConfigured
