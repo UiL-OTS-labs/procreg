931f4b7e428eed1547df1a8df0093ec07a2ae87d
Michael Villeneuve <m.l.villeneuve@uu.nl>
2023-09-19 14:04:03 +0200

    Add migrations and admin widgets for Faq and Faqlists
    FaqList needs TinyMCE for its help text and descriptions

--- registrations/models/faq.py
+++ registrations/models/faq.py
@@ -61,6 +61,12 @@ class FaqList(models.Model):
     faqs = models.ManyToManyField(
         "registrations.Faq",
     )
+    show_help_text = models.BooleanField(
+        default=True,
+    )
+    help_text = models.TextField(
+        default="",
+    )
 
     def __str__(self):
         return self.slug
