ad011db2d3d70c08bd82490fc0ced9591bed76f4
Michael Villeneuve <m.l.villeneuve@uu.nl>
2023-11-14 12:42:09 +0100

    fix: locale settings, set English as default internal to Django

--- procreg/settings/develop.py
+++ procreg/settings/develop.py
@@ -284,20 +284,14 @@ LANGUAGES = (
     ('en', _('lang:en')),
 )
 
-
+LOCALE_PATHS = (
+    'locale',
+)
 
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-# LOCALE_PATHS = (
-#     'locale',
-#     (BASE_DIR / "locale"),
-# )
 
 
 # Static files (CSS, JavaScript, Images)
